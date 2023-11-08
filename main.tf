e3f3f4e3491e34e79066fba92ddc03e67c2509d9
MalekFarhani <92673668+MalekFarhani@users.noreply.github.com>
2023-11-08 18:54:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,8 @@ resource "azurerm_kubernetes_cluster" "prodk8s" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.prodk8s.kube_config_raw
+  value     = azurerm_kubernetes_cluster.prodk8s.kube_config_raw
+  sensitive = true
 }
 
 output "cluster_name" {

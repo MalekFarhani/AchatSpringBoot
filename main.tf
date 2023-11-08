bf92f2d89dac97b393cf968cf9b0dde058afe40a
MalekFarhani <92673668+MalekFarhani@users.noreply.github.com>
2023-11-08 21:04:00 +0100

    s

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "prod" {
-  name     = "myRessourceGroup"
+  name     = "mRessourceGroup"
   location = "East US"
 }
 
 resource "azurerm_kubernetes_cluster" "prodk8s" {
-  name                = "myAKSCluster"
+  name                = "mAKSCluster"
   location            = azurerm_resource_group.prod.location
   resource_group_name = azurerm_resource_group.prod.name
   dns_prefix          = "myaksdns"
